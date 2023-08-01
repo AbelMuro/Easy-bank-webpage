c0f5a5bc38b8510136f14f7a52a587f33405ef8d
abelmuro <abelmuro93@gmail.com>
2023-08-01 13:37:59 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import {motion} from 'framer-motion';
+import './styles.css';
+
+
 
 function App() {
-    return(<>hello app</>)
+
+
+    return(
+        <motion.div className={'circle'} initial={{opacity: 0, backgroundColor: 'green'}} whileInView={{opacity: 1, backgroundColor: 'white'}} />  
+
+    )
 }
 
 export default App;
