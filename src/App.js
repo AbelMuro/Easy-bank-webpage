3ac59d3d94a0c0c3a03f6c9a6c0e2213bd7e5958
abel muro <abelmuro93@gmail.com>
2023-08-02 21:15:27 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React, {useState, useEffect} from 'react';
-import {motion, useMotionValue, useTransform, useAnimate} from 'framer-motion';
+import {motion, LayoutGroup} from 'framer-motion';
 import './styles.css';
 
+
 function App() {
-    const count = useMotionValue(0);
-    const rounded = useTransform(count, latest => Math.round(latest));
-    const [,animate] = useAnimate();
-    
-    useEffect(() => {
-      const controls = animate(count, 100)
-    
-      return controls.stop
-    }, [])
-    
-    return <motion.div>
-            {rounded}
-        </motion.div>
-  
+  const [isSelected, setIsSelected] = useState(false)
+
+  const handleClick = () => {
+    setIsSelected(!isSelected);
+  }
+
+  return (
+    <div className={'container'}>
+        <div className={'box'}>
+          {isSelected && <motion.div className={'lineOne'} layoutId="underline" />  } 
+        </div>
+        <div className={'box'}>
+          {!isSelected && <motion.div className={'lineTwo'} layoutId="underline" />  }       
+        </div>
+        <button onClick={handleClick}>
+          click me
+        </button>
+    </div>
+
+  )
 }
+    
 
 export default App;
