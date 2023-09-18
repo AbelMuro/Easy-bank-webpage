13a5363cb3b3b528b33c5e0d7aaf4b78915e6932
abelmuro <abelmuro93@gmail.com>
2023-09-18 11:03:45 -0700

    commit

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -20,35 +20,37 @@ function Header() {
     }
 
     return(
-        <header className={styles.container}>
-            <motion.section className={styles.header} 
-                initial='hidden' 
-                animate='show'
-                transition={{staggerChildren: 0.6}}>
-                    <motion.h1 className={styles.title} variants={variantsChildren}>
-                        Next generation digital banking
-                    </motion.h1>
-                    <motion.p className={styles.desc} variants={variantsChildren}>
-                        Take your financial life online. 
-                        Your Easybank account will be a one-stop-shop for spending, 
-                        saving, budgeting, investing, and much more.
-                    </motion.p>
-                    <RequestButton variants={variantsChildren}/> 
-            </motion.section>
+        <>
+            <header className={styles.container}>
+                <motion.section className={styles.header} 
+                    initial='hidden' 
+                    animate='show'
+                    transition={{staggerChildren: 0.6}}>
+                        <motion.h1 className={styles.title} variants={variantsChildren}>
+                            Next generation digital banking
+                        </motion.h1>
+                        <motion.p className={styles.desc} variants={variantsChildren}>
+                            Take your financial life online. 
+                            Your Easybank account will be a one-stop-shop for spending, 
+                            saving, budgeting, investing, and much more.
+                        </motion.p>
+                        <RequestButton variants={variantsChildren}/> 
+                </motion.section>
+                <motion.img 
+                    className={styles.phone} 
+                    src={images['phones']}
+                    initial={{y: -400, opacity: 0}} 
+                    animate={{y: 0, opacity: 1}}
+                    transition={{duration: 1.7}}/>                      
+            </header>        
             <motion.img 
-                className={styles.phone} 
-                src={images['phones']}
-                initial={{y: -400, opacity: 0}} 
-                animate={{y: 0, opacity: 1}}
-                transition={{duration: 1.7}}/>                      
+                    className={styles.backgroundImage} 
+                    src={images[mobile ? 'backgroundImageMobile' : 'backgroundImage']}
+                    initial={{x: 400, opacity: 0}}
+                    animate={{x: 0, opacity: 1}}
+                    transition={{duration: 1.3}}/>                
+        </>
 
-            <motion.img 
-                className={styles.backgroundImage} 
-                src={images[mobile ? 'backgroundImageMobile' : 'backgroundImage']}
-                initial={{x: 400, opacity: 0}}
-                animate={{x: 0, opacity: 1}}
-                transition={{duration: 2.3}}/>                
-        </header>
     )
 }
 
