abd25f417bf89afa4679533a19f1437904defcf1
abelmuro <abelmuro93@gmail.com>
2023-09-18 13:42:42 -0700

    commit

--- src/NavBar/MobileNavBar/MobileNavBar.js
+++ src/NavBar/MobileNavBar/MobileNavBar.js
@@ -4,7 +4,21 @@ import MenuIcon from './MenuIcon';
 import styles from './styles.module.css';
 import {motion} from 'framer-motion';
 
+//i need to do some refactoring here
 function MobileNavBar() {
+
+    const variants = {
+        hidden: {
+            y: -150, 
+            opacity: 0
+        },
+        show: {
+            y: 0, 
+            opacity: 1,
+            transition: {duration: 0.4}
+        } 
+    }
+
     return(
             <motion.div 
                 className={styles.container} 
